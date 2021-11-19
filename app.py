6682b678b1b2ee8eb0dcac1fbfcd0de7441beebb
jonathanj101 <jonatjimen@gmail.com>
2021-11-18 22:03:34 -0500

    implement class for card validation logic, not done or tested

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 
+# class Card_Validation():
+#     def __init__(self, card_number):
+#         # check if card consist of 16 digits
+#         self.card_number = card_number
+
+#         if card_number == 16:
+#             pass
+#         else:
+#             pass
+
+#     def is_card_numbers_only(self):
+#         # check if input contain only number
+#         if int(self.card_number):
+#             pass
+#         else:
+#             pass
+#     def card_starting_number(self):
+#         # check if card number starts with 9 or 7 or 3
+#         pass
+
+
+
 window = Tk()
 window.geometry("1000x500")
 window.title("Credit Card Validator")
